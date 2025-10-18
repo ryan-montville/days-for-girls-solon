fefcfb36bd0372fb069db02d238bdc1c3baaa87e
Ryan Montville <rmontville@luc.edu>
2025-10-18 11:25:59 -0500

    Starting to work on inventory report functions

--- assets/inventory.js
+++ assets/inventory.js
@@ -1,11 +1,21 @@
+const currentInventoryLocalStorage = localStorage.getItem("currentInventory");
+let currentInventoryData = JSON.parse(currentInventoryLocalStorage);
+const incomingInventoryLocalStorage = localStorage.getItem("incomingInventory");
+let incomingInventoryData = JSON.parse(incomingInventoryLocalStorage);
+const outgoingInventoryLocalStorage = localStorage.getItem("outgoingInventory");
+let outgoingInventoryData = JSON.parse(outgoingInventoryLocalStorage);
+
 //page elements
 const errorMessage = document.getElementById('error');
 const currentInventoryTableBody = document.getElementById('current-inventory-body');
 const manageInventoryButtons = document.getElementById('manage-inventory');
-const inventoryReport = document.getElementById('inventory-report');
-const currentInventoryLocalStorage = localStorage.getItem("currentInventory");
-const incomingInventoryLocalStorage = localStorage.getItem("incomingInventory");
-const outgoingInventoryLocalStorage = localStorage.getItem("outgoingInventory");
+const inventoryReportCard = document.getElementById('inventory-report-card');
+const generateForm = document.getElementById('generate-form');
+const startDateInput = document.getElementById('start-date');
+const endDateInput = document.getElementById('end-date');
+const generateButton = document.getElementById('generate-button');
+
+
 
 function createErrorMessage(error) {
     let p = document.createElement('p');
@@ -23,27 +33,119 @@ function addItemToTable(component, tableName) {
         let componentQuantityCell = newRow.insertCell();
         let componentQuantity = document.createTextNode(component.quantity);
         componentQuantityCell.appendChild(componentQuantity);
-    } else if (tableName === "incomingInventory") {
-        
-    } else if (tableName === "outgoingInventory") {
-
     } else {
         console.log(`tableName param not right: ${tableName}`);
     }
 }
 
 function loadCurrentInventory() {
-    let currentInventoryData = JSON.parse(currentInventoryLocalStorage);
     for (let i = 0; i < currentInventoryData.length; i++) {
         addItemToTable(currentInventoryData[i], "currentInventory");
     }
+}
+
+function filterDateRange(startDate, endDate) {
+    let filteredIncoming = incomingInventoryData.filter(item => startDate <= new Date(item.date) <= endDate);
+    let filteredOutgoing = outgoingInventoryData.filter(item => startDate <= new Date(item.date) <= endDate);
+    //combine incoming and outgoing items, convert quantity to positive number for incoming and negative number for outgoing
+    let filteredResults = [];
+    for (let i = 0; i < filteredIncoming.length; i++) {
+        let currentItem = filteredIncoming[i];
+        let newItem = {
+            "date": currentItem.date,
+            "componentType": currentItem.componentType,
+            "quantity": currentItem.quantity
+        };
+        filteredResults.push(newItem);
+    }
+    for (let i = 0; i < filteredOutgoing.length; i++) {
+        let currentItem = filteredOutgoing[i];
+        let newItem = {
+            "date": currentItem.date,
+            "componentType": currentItem.componentType,
+            "quantity": currentItem.quantity * -1
+        };
+        filteredResults.push(newItem);
+    }
+    return filteredResults;
+}
+
+function createSummaryTable(filteredArray) {
+    const summaryTable = document.createElement('table');
+    //table header
+    const summaryThead = document.createElement('thead');
+    const summaryHeaderRow = document.createElement('tr');
+    const donatedHeader = document.createElement('th');
+    donatedHeader.textContent = "Donated";
+    summaryHeaderRow.appendChild(donatedHeader);
+    const distributedHeader = document.createElement('th');
+    distributedHeader.textContent = "Distributed";
+    summaryHeaderRow.appendChild(distributedHeader);
+    summaryThead.appendChild(summaryHeaderRow)
+    summaryTable.appendChild(summaryThead);
+    //table body
+
+    return summaryTable;
+}
+
+function generateReport() {
+    let reportContainer = document.createElement('div');
+    //Create inventory report h3 with date range
+    /* I learned how to fix the date being off by one from these
+    stackOverflow threads: https://stackoverflow.com/questions/7556591/is-the-javascript-date-object-always-one-day-off
+    and https://stackoverflow.com/questions/2035699/how-to-convert-a-full-date-to-a-short-date-in-javascript */
+    let startDate = new Date(startDateInput.value);
+    let startDateTimezoneFixed = new Date(startDate.getTime() - startDate.getTimezoneOffset() * -60000);
+    let startDateFormatted = startDateTimezoneFixed.toLocaleDateString('en-US', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+    });
+    let endDate = new Date(endDateInput.value);
+    let endDateTimezoneFixed = new Date(endDate.getTime() - endDate.getTimezoneOffset() * -60000);
+    let endDateFormatted = endDateTimezoneFixed.toLocaleDateString('en-US', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+    });
+    let reportTitleH3 = document.createElement('h3');
+    let reportTitle = document.createTextNode(`Inventory report for ${startDateFormatted} to ${endDateFormatted}`);
+    reportTitleH3.appendChild(reportTitle);
+    reportContainer.appendChild(reportTitleH3);
+    //Create list of entries within date range
+    let filteredResults = filterDateRange(startDate, endDate);
+    //create table to summarize all entries
+    const summaryTable = createSummaryTable(filteredResults);
+    reportContainer.appendChild(summaryTable);
+    //Generate table for all entries in date range
+
+    //Create button to generate new inventory report
+    const formRow = document.createElement('div');
+    formRow.setAttribute('class', 'form-row');
+    const newReportButton = document.createElement('button')
+    newReportButton.setAttribute('class', 'button-full');
+    newReportButton.textContent = "Generate New Report";
+    newReportButton.addEventListener('click', () => {
+        reportContainer.remove();
+        generateForm.style.display = 'block';
+    });
+    formRow.appendChild(newReportButton);
+    reportContainer.appendChild(formRow);
+    inventoryReportCard.appendChild(reportContainer);
 }
 
 //check to see if the user is signed in, if not remove links to manage inventory and generate reports
 let localStorageUser = localStorage.getItem("username");
 if (!localStorageUser) {
     manageInventoryButtons.remove();
-    inventoryReport.remove();
+    inventoryReportCard.remove();
 }
 
 loadCurrentInventory();
+
+generateButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    generateReport();
+    generateForm.reset();
+    generateForm.style.display = 'none';
+});
