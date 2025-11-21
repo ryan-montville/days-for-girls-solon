f94ce9db42168f331e0529fb24c691ae48f6e530
Ryan Montville <rmontville@luc.edu>
2025-11-21 13:05:52 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  base: '/days-for-girls-solon/',
   build: {
+    outDir: 'dist',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
