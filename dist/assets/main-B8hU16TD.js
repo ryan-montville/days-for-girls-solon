b4672452566e1989215f17c5a76dfd70ecba2909
Ryan Montville <rmontville@luc.edu>
2025-11-21 13:01:10 -0600

    changing script tags and updatin vite config

---
+++ dist/assets/main-B8hU16TD.js
@@ -0,0 +1,1 @@
+import{i}from"./app-DLSjkG7u.js";i("Home");console.log("testing");
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  root: './',
   build: {
-    target: 'esnext',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        createNewEvent: resolve(__dirname, 'create-new-event.html'),
+        distributedInventory: resolve(__dirname, 'distributed-inventory.html'),
+        donate: resolve(__dirname, 'donate.html'),
+        donatedInventory: resolve(__dirname, 'donated-inventory.html'),
+        eventSignUp: resolve(__dirname, 'event-sign-up.html'),
+        events: resolve(__dirname, 'events.html'),
+        inventory: resolve(__dirname, 'inventory.html'),
+        mailingList: resolve(__dirname, 'mailing-list.html'),
+        manageEvent: resolve(__dirname, 'manage-event.html'),
+      },
+    },
   },
 });
